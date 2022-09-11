e39c33f52b088983df5485e59f411a87b6cb6920
BIBI78 <blakecollins78@gmail.com>
2022-09-11 16:41:25 +0000

    Add second roll to compare function

--- assets/js/script.js
+++ assets/js/script.js
@@ -166,11 +166,36 @@
           const fifthNumber = Math.floor(Math.random()*6) + 1;
           const sixthNumber = Math.floor(Math.random()*6) + 1;
 
+// 
+
+const firstDie =  "assets/images/d" + firstNumber + ".png";
+const secondDie =  "assets/images/d" + secondNumber + ".png";
+const thirdDie =  "assets/images/d" + thirdNumber + ".png";
+const fourthDie =  "assets/images/d" + fourthNumber + ".png";
+const fifthDie =  "assets/images/d" + fifthNumber + ".png";
+const sixthDie =  "assets/images/d" + sixthNumber + ".png";
+
+document.getElementsByTagName('img')[0].setAttribute("src",firstDie); 
+document.getElementsByTagName('img')[1].setAttribute("src",secondDie); 
+document.getElementsByTagName('img')[2].setAttribute("src",thirdDie); 
+document.getElementsByTagName('img')[3].setAttribute("src",fourthDie); 
+document.getElementsByTagName('img')[4].setAttribute("src",fifthDie); 
+document.getElementsByTagName('img')[5].setAttribute("src",sixthDie); 
+
+
+
+          // 
+
+
+
+
+
+
           const myRollOne = [firstNumber,secondNumber,thirdNumber];
           console.log(myRollOne);
-          console.log(firstNumber);
-          console.log(secondNumber);
-          console.log(thirdNumber);
+          // console.log(firstNumber);
+          // console.log(secondNumber);
+          // console.log(thirdNumber);
 
           const myRollTwo = [fourthNumber,fifthNumber,sixthNumber];
           // console.log(myRollTwo);
@@ -203,6 +228,9 @@
           //           }
           //         }
 
+
+
+          // HOW DO I COMAPRE ROLL 1 and ROLL 2 ??
           }
           
 
@@ -210,10 +238,111 @@
 
       }
 
+function compare3 (){
+
+          const firstNumber = Math.floor(Math.random()*6) + 1;
+          const secondNumber = Math.floor(Math.random()*6) + 1;
+          const thirdNumber = Math.floor(Math.random()*6) + 1;
+
+         
 
 
 
-    
+const firstDie =  "assets/images/d" + firstNumber + ".png";
+const secondDie =  "assets/images/d" + secondNumber + ".png";
+const thirdDie =  "assets/images/d" + thirdNumber + ".png";
+
+
+
+document.getElementsByTagName('img')[0].setAttribute("src",firstDie); 
+document.getElementsByTagName('img')[1].setAttribute("src",secondDie); 
+document.getElementsByTagName('img')[2].setAttribute("src",thirdDie);
+
+
+
+
+
+          
+
+          const myRollOne = [firstNumber,secondNumber,thirdNumber];
+          console.log(myRollOne);
+        
+
+      
+
+// LET ME SET CONDITONS FIRST
+          if ((myRollOne[0] === 1 || myRollOne[0] === 2 || myRollOne[0] === 3) && (myRollOne[0] !== myRollOne[1]) && 
+(myRollOne[1] === 1 || myRollOne[1] === 2 || myRollOne[1] === 3 )&& (myRollOne[1] !== myRollOne[2]) &&
+(myRollOne[2] === 1 || myRollOne[2] === 2 || myRollOne[2] === 3) && (myRollOne[2] !== myRollOne[0])){
+  // console.log("automatic lose")
+  const rollOneValue = myRollOne;
+console.log("automatic lose"+rollOneValue);
+
+
+
+// i want to add like a STOP here and get out of the code
+
+
+              } else if ((myRollOne[0] === 4 || myRollOne[0] === 5 || myRollOne[0] === 6) && (myRollOne[0] !== myRollOne[1]) && 
+(myRollOne[1] === 4 || myRollOne[1] === 5 || myRollOne[1] === 6 )&& (myRollOne[1] !== myRollOne[2]) &&
+(myRollOne[2] === 4 || myRollOne[2] === 5 || myRollOne[2] === 6) && (myRollOne[2] !== myRollOne[0])){
+  console.log("automatic win")
+
+              } else if (myRollOne[0] === myRollOne[1] && myRollOne[1] === myRollOne[2]){
+// console.log(" all the same")
+const rollOneValue = myRollOne[0];
+console.log("all the same "+ rollOneValue +"s");
+
+
+// second roll starts here 
+
+const fourthNumber = Math.floor(Math.random()*6) + 1;
+const fifthNumber = Math.floor(Math.random()*6) + 1;
+const sixthNumber = Math.floor(Math.random()*6) + 1;
+
+const fourthDie =  "assets/images/d" + fourthNumber + ".png";
+const fifthDie =  "assets/images/d" + fifthNumber + ".png";
+const sixthDie =  "assets/images/d" + sixthNumber + ".png";
+
+document.getElementsByTagName('img')[3].setAttribute("src",fourthDie); 
+document.getElementsByTagName('img')[4].setAttribute("src",fifthDie); 
+document.getElementsByTagName('img')[5].setAttribute("src",sixthDie); 
+
+
+const myRollTwo = [fourthNumber,fifthNumber,sixthNumber];
+console.log(myRollTwo);
+
+if ((myRollTwo[0] === 1 || myRollTwo[0] === 2 || myRollTwo[0] === 3) && (myRollTwo[0] !== myRollTwo[1]) && 
+(myRollTwo[1] === 1 || myRollTwo[1] === 2 || myRollTwo[1] === 3 )&& (myRollTwo[1] !== myRollTwo[2]) &&
+(myRollTwo[2] === 1 || myRollTwo[2] === 2 || myRollTwo[2] === 3) && (myRollTwo[2] !== myRollTwo[0])){
+  // console.log("automatic lose")
+  const rollTwoValue = myRollTwo;
+console.log("automatic lose"+rollTwoValue);
+
+
+// SSTOP
+
+
+              } else if ((myRollTwo[0] === 4 || myRollTwo[0] === 5 || myRollTwo[0] === 6) && (myRollTwo[0] !== myRollTwo[1]) && 
+(myRollTwo[1] === 4 || myRollTwo[1] === 5 || myRollTwo[1] === 6 )&& (myRollTwo[1] !== myRollTwo[2]) &&
+(myRollTwo[2] === 4 || myRollTwo[2] === 5 || myRollTwo[2] === 6) && (myRollTwo[2] !== myRollTwo[0])){
+  console.log("automatic win")
+
+              } else if (myRollTwo[0] === myRollTwo[1] && myRollTwo[1] === myRollTwo[2]){
+// console.log(" all the same")
+const rollTwoValue = myRollTwo[0];
+console.log("all the same "+ rollTwoValue +"s");
+
+
+
+
+
+
+}
+
+}
+
+
+}
      
    
- 
