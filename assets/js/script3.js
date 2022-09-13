4a057bdd0552c89fb4f0c8b8f9cad67fd13db5fb
BIBI78 <blakecollins78@gmail.com>
2022-09-13 15:05:02 +0000

    ive confused myself combining the two roll functions , compare doesnt work

---
+++ assets/js/script3.js
@@ -0,0 +1,14 @@
+
+
+const randomArr = [1, 2, 3]
+const userRolled = [1, 2 ,3]
+
+function checkNumbers () {
+    const thesame = algorithm
+    if(thesame){
+        console.log('the same')
+    }else if( userRolled ===  randomArr) {
+        
+    }
+}
+
