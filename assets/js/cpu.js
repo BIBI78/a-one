ebf84b299c31c403481cd7468bc0d9e2803d0df3
BIBI78 <blakecollins78@gmail.com>
2022-09-19 15:23:16 +0000

    add corrections to spin

--- assets/js/cpu.js
+++ assets/js/cpu.js
@@ -6,7 +6,8 @@ let compareValueTwo = 2;
 //** this is my original spin function. but i thnk theres is a better way to spin the image. i also want to add it to my roll function */
 function spin() {
   document.getElementById('img').className = 'spinImg'
-
+  document.getElementById('img2').className = 'spinImg'
+  document.getElementById('img3').className = 'spinImg'
 
 
 }
@@ -15,104 +16,35 @@ function spin() {
 
 const snake = document.getElementById('img');
 
+const snake2 = document.getElementById('img2');
+const snake3 = document.getElementById('img3');
+
 
 function removeSpinClass() {
   snake.classList.remove('spinImg');
-  //  these are for the future die
-  //  snake1.classList.remove('spinImg');
-  //  snake2.classList.remove('spinImg');
+
+  snake2.classList.remove('spinImg');
+  snake3.classList.remove('spinImg');
 
 
 }
 
 snake.addEventListener('animationend', removeSpinClass);
+snake2.addEventListener('animationend', removeSpinClass);
+snake3.addEventListener('animationend', removeSpinClass);
+
+
+
+
+
+
+
+
 
 //
 
 // this is the first rollDie fucntion , this gets a random number between 1-6 and switches the die image acordingly 
 
-function rollDie() {
-  const firstNumber = Math.floor(Math.random() * 6) + 1;
-  const secondNumber = Math.floor(Math.random() * 6) + 1;
-  const thirdNumber = Math.floor(Math.random() * 6) + 1;
-
-
-
-  const firstDie = "assets/images/d" + firstNumber + ".png";
-  const secondDie = "assets/images/d" + secondNumber + ".png";
-  const thirdDie = "assets/images/d" + thirdNumber + ".png";
-
-  document.getElementsByTagName('img')[0].setAttribute("src", firstDie);
-  document.getElementsByTagName('img')[1].setAttribute("src", secondDie);
-  document.getElementsByTagName('img')[2].setAttribute("src", thirdDie);
-
-  console.log(firstNumber);
-  console.log(secondNumber);
-  console.log(thirdNumber);
-  console.log("first roll");
-
-}
-
-// same as above but for palyer 2.
-function rollDieTwo() {
-
-  const fourthNumber = Math.floor(Math.random() * 6) + 1;
-  const fifthNumber = Math.floor(Math.random() * 6) + 1;
-  const sixthNumber = Math.floor(Math.random() * 6) + 1;
-
-
-
-  const fourthDie = "assets/images/d" + fourthNumber + ".png";
-  const fifthDie = "assets/images/d" + fifthNumber + ".png";
-  const sixthDie = "assets/images/d" + sixthNumber + ".png";
-
-  document.getElementsByTagName('img')[3].setAttribute("src", fourthDie);
-  document.getElementsByTagName('img')[4].setAttribute("src", fifthDie);
-  document.getElementsByTagName('img')[5].setAttribute("src", sixthDie);
-
-  console.log(fourthNumber);
-  console.log(fifthNumber);
-  console.log(sixthNumber);
-  console.log("second roll");
-
-
-}
-
-
-// i just added the the functions together here.
-
-function rollBoth() {
-  const firstNumber = Math.floor(Math.random() * 6) + 1;
-  const secondNumber = Math.floor(Math.random() * 6) + 1;
-  const thirdNumber = Math.floor(Math.random() * 6) + 1;
-  const fourthNumber = Math.floor(Math.random() * 6) + 1;
-  const fifthNumber = Math.floor(Math.random() * 6) + 1;
-  const sixthNumber = Math.floor(Math.random() * 6) + 1;
-
-
-  const firstDie = "assets/images/d" + firstNumber + ".png";
-  const secondDie = "assets/images/d" + secondNumber + ".png";
-  const thirdDie = "assets/images/d" + thirdNumber + ".png";
-  const fourthDie = "assets/images/d" + fourthNumber + ".png";
-  const fifthDie = "assets/images/d" + fifthNumber + ".png";
-  const sixthDie = "assets/images/d" + sixthNumber + ".png";
-
-  document.getElementsByTagName('img')[0].setAttribute("src", firstDie);
-  document.getElementsByTagName('img')[1].setAttribute("src", secondDie);
-  document.getElementsByTagName('img')[2].setAttribute("src", thirdDie);
-  document.getElementsByTagName('img')[3].setAttribute("src", fourthDie);
-  document.getElementsByTagName('img')[4].setAttribute("src", fifthDie);
-  document.getElementsByTagName('img')[5].setAttribute("src", sixthDie);
-
-  console.log(firstNumber);
-  console.log(secondNumber);
-  console.log(thirdNumber);
-  console.log(fourthNumber);
-  console.log(fifthNumber);
-  console.log(sixthNumber);
-  console.log("free YSL");
-
-}
 
 // the sound is delayed  here, this is a mp3 file for the clicking sound , i want to replace this zith a dice roll sound.
 function play() {
@@ -121,7 +53,7 @@ function play() {
 }
 
 
-//  help
+
 
 
 function p1Roll(){
